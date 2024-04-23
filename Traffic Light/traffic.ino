aa5c83362f40de167c1b970cb048eedcb77f8451
Shaon Samanta <156128187+RyukiDoesIt@users.noreply.github.com>
2024-04-24 01:32:19 +0530

    Updated traffic.ino

--- Traffic Light/traffic.ino
+++ Traffic Light/traffic.ino
@@ -1,20 +1,69 @@
-void setup() {
-    pinMode(24,OUTPUT);
-    pinMode(32,OUTPUT);
-    pinMode(42,OUTPUT);
+int red = 9;
+int yellow = 8;
+int green = 7;
+
+void setup(){
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green,  OUTPUT);
 }
 
-void loop() {
-    digitalWrite(24,HIGH);
-    delay(1000);
-    digitalWrite(24,LOW);
-    delay(500);
-    digitalWrite(32,HIGH);
-    delay(1000);
-    digitalWrite(32,LOW);
-    delay(500);
-    digitalWrite(42,HIGH);
-    delay(1000);
-    digitalWrite(42,LOW);
-    delay(500);
+void loop(){
+  digitalWrite(red, HIGH);
+  delay(15000);
+  digitalWrite(red,  LOW);
+  
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow,  LOW);
+  delay(500);
+
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow,  LOW);
+  delay(500);
+
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow,  LOW);
+  delay(500);
+  
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow, LOW);
+  delay(500);
+  
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow, LOW);
+  delay(500);
+  
+  digitalWrite(green, HIGH);
+  delay(20000);
+  digitalWrite(green,  LOW);
+  
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow,  LOW);
+  delay(500);
+
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow,  LOW);
+  delay(500);
+
+  digitalWrite(yellow, HIGH);
+  delay(1000);  
+  digitalWrite(yellow, LOW);
+  delay(500);
+  
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow, LOW);
+  delay(500);
+  
+  digitalWrite(yellow, HIGH);
+  delay(1000);
+  digitalWrite(yellow, LOW);
+  delay(500);
 }
