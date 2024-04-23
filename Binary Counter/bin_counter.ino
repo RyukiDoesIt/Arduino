f2c0d2846b7a07d0f5cfc29f20f0135a29d9fa4e
Shaon Samanta <156128187+RyukiDoesIt@users.noreply.github.com>
2024-04-24 01:34:16 +0530

    Updated bin_counter.ino

--- Binary Counter/bin_counter.ino
+++ Binary Counter/bin_counter.ino
@@ -20,49 +20,49 @@ void loop() {
     Serial.println(volt);
     delay(t);
 
-    if(volt >= 7.9) {
+    if(volt>=7.9) {
         digitalWrite(b0, LOW);
         digitalWrite(b1, LOW);
         digitalWrite(b2, LOW);
         digitalWrite(b3, HIGH);
     }
-    else if(volt >= 6.9) {
+    else if(volt>=6.9) {
         digitalWrite(b0, HIGH);
         digitalWrite(b1, HIGH);
         digitalWrite(b2, HIGH);
         digitalWrite(b3, LOW);
     }
-    else if(volt >= 5.9) {
+    else if(volt>=5.9) {
         digitalWrite(b0, LOW);
         digitalWrite(b1, HIGH);
         digitalWrite(b2, HIGH);
         digitalWrite(b3, LOW);
     }
-    else if(volt >= 4.9) {
+    else if(volt>=4.9) {
         digitalWrite(b0, HIGH);
         digitalWrite(b1, LOW);
         digitalWrite(b2, HIGH);
         digitalWrite(b3, LOW);
     }
-    else if(volt >= 3.9) {
+    else if(volt>=3.9) {
         digitalWrite(b0, LOW);
         digitalWrite(b1, LOW);
         digitalWrite(b2, HIGH);
         digitalWrite(b3, LOW);
     }
-    else if(volt >= 2.9) {
+    else if(volt>=2.9) {
         digitalWrite(b0, HIGH);
         digitalWrite(b1, HIGH);
         digitalWrite(b2, LOW);
         digitalWrite(b3, LOW);
     }
-    else if(volt >= 1.9) {
+    else if(volt>=1.9) {
         digitalWrite(b0, LOW);
         digitalWrite(b1, HIGH);
         digitalWrite(b2, LOW);
         digitalWrite(b3, LOW);
     }
-    else if(volt >= 0.9) {
+    else if(volt>=0.9) {
         digitalWrite(b0, HIGH);
         digitalWrite(b1, LOW);
         digitalWrite(b2, LOW);
